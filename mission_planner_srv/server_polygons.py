6f4b3f526ffb4aaeb9230b0aedb10427f8c67641
Erez Karpas <karpase@gmail.com>
2023-06-12 10:58:19 +0300

    more elegant polygon stuff

--- mission_planner_srv/server_polygons.py
+++ mission_planner_srv/server_polygons.py
@@ -1,5 +1,5 @@
 from swarm_interfaces.srv import MissionPlanningV2
-from swarm_interfaces.msg import Mission, PlannedMissionV2, GeoPoint, Zone
+from swarm_interfaces.msg import Mission, PlannedMissionV2, GeoPoint, Zone, ZoneTypeEnum
 import shapely
 import numpy as np
 from typing import List
@@ -10,10 +10,36 @@ from rclpy.node import Node
 publish_plan_topic = "mission_plan"
 
 class GridCellData:
-    def __init__(self, target_priority : List[int] = [], target_detection_probability : List[float] = [], blocked : bool = False):
-        self.target_priority = target_priority
-        self.target_detection_probability = target_detection_probability
-        self.blocked = blocked
+    def __init__(self, area):
+        self._target_data = {}        
+        self._blocked = False
+        self._area = area
+
+    @property
+    def blocked(self):
+        return self._blocked
+    
+    @blocked.setter
+    def blocked(self, value):
+        self._blocked = value
+
+    @property
+    def target_data(self):
+        return self._target_data
+
+    def update_target_data(self, target_priority : List[int] = [], target_detection_probability : List[float] = []):
+        for priority, probability in zip(target_priority, target_detection_probability):
+            if priority in self._target_data:
+                assert(self._target_data[priority] == probability)
+            self._target_data[priority] = probability
+
+
+    @property
+    def area(self):
+        return self._area
+
+    def __repr__(self):
+        return "area:" + str(self.area) + "__blocked:" + str(self.blocked) + "__target:" + str(self.target_data)
 
 
 class MissionPlanningService(Node):
@@ -27,13 +53,12 @@ class MissionPlanningService(Node):
     def create_grid(self, delta=0.5, minx = 0, maxx = 10, miny = 0, maxy = 10):
         nx = int((maxx - minx)/delta)+1 
         ny = int((maxy - miny)/delta)+1 
-        gx, gy = np.linspace(minx,maxx,nx), np.linspace(miny,maxy,ny)
-        print(nx, ny, gx, gy)
-        grid = []
+        gx, gy = np.linspace(minx,maxx,nx), np.linspace(miny,maxy,ny)       
+        grid = {}
         for i in range(len(gx)-1):
             for j in range(len(gy)-1):
                 poly_ij = shapely.Polygon([[gx[i],gy[j]],[gx[i],gy[j+1]],[gx[i+1],gy[j+1]],[gx[i+1],gy[j]]])
-                grid.append( (i, j, poly_ij ) )
+                grid[i,j] = GridCellData(poly_ij)
         return grid
 
 
@@ -42,13 +67,16 @@ class MissionPlanningService(Node):
         altitude = mission.initial_location.altitude + 1        
         grid = self.create_grid()
         for zone in mission.zones:
-            polygon = shapely.Polygon( map(lambda geopoint: (geopoint.latitude, geopoint.longitude), zone.geo_points))
-            print(polygon)
-            for i,j,cell in grid:
-                if polygon.intersects(cell):
-                    print(polygon, cell, i, j)
-            
-
+            polygon = shapely.Polygon( map(lambda geopoint: (geopoint.latitude, geopoint.longitude), zone.geo_points))            
+            for i,j in grid:
+                cell = grid[i,j]
+                if polygon.intersects(cell.area):
+                    if zone.zone_type.value == ZoneTypeEnum.FLIGHT_FORBIDDEN_ZONE:
+                        cell.blocked = True
+                    elif zone.zone_type.value == ZoneTypeEnum.TARGET_DATA_ZONE:                        
+                        cell.update_target_data(zone.target_data.target_priority, zone.target_data.target_detection_probability)                        
+        for x, y in grid:
+            print(x, y, grid[x,y])
 
         plan = []
 
